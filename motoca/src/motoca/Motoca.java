dade94755c5ebc0a52c4392c0ac817f7997a0af0
abnerhakinnen <abnerhakinnen@alu.ufc.br>
2020-03-05 19:07:55 -0300

    atividade1

--- motoca/src/motoca/Motoca.java
+++ motoca/src/motoca/Motoca.java
@@ -1,64 +1,72 @@
 package motoca;
 
 class Pessoa {
+
     String nome;
     int idade;
-    
-    
-    public Pessoa(String nome, int idade){ //nao eh metodo, eh invocado automaticaticamente na criacao
+
+    public Pessoa(String nome, int idade) { //nao eh metodo, eh invocado automaticaticamente na criacao
         this.nome = nome;
         this.idade = idade;
     }
 }
 
 public class Motoca {
-	Pessoa pessoa;
 
-	public Motoca(){
-	}
-	
-	void embarcar(Pessoa pessoa) {
-		if(this.pessoa == null)
-			this.pessoa = pessoa;
-		else
-			System.out.println("Ja tem gente na motoca");
-	}
-	
-	void desembarcar() {
-		if(this.pessoa != null)
-			this.pessoa = null;
-		else
-			System.out.println("Nao tem ninguem na moto");
-	}
+    Pessoa pessoa;
+    int potencia = 3;
 
-	void dirigir(){
-		if(this.pessoa == null)
-			System.out.println("Nao tem ninguem na moto");
-		else if(this.pessoa.idade < 2)
-			System.out.println("Muito pequeno pra andar de moto");
-		else if(this.pessoa.idade > 10)
-			System.out.println("Muito grande pra andar de moto");
-		else
-			System.out.println(this.pessoa.nome + ": Run...");
-	}
+    public Motoca() {
+        this.potencia = potencia;
+    }
 
+    void embarcar(Pessoa pessoa) {
+        if (this.pessoa == null) {
+            this.pessoa = pessoa;
+        } else {
+            System.out.println("Ja tem gente na motoca");
+        }
+    }
 
-	public static void main(String[] args) {
-		Pessoa davi = new Pessoa("Davi", 10);
-		{
-			Motoca moto = new Motoca();
-			moto.embarcar(davi);
-			moto.dirigir();
-			moto.desembarcar();
-		}
-		Motoca moto2 = new Motoca();
-		moto2.embarcar(davi);
-		moto2.dirigir();
-		moto2.desembarcar();
+    void desembarcar() {
+        if (this.pessoa != null) {
+            this.pessoa = null;
+        } else {
+            System.out.println("Nao tem ninguem na moto");
+        }
+    }
 
-		System.out.println(davi.nome);
-	}
-/*
+    void dirigir() {
+        if (this.pessoa == null) {
+            System.out.println("Nao tem ninguem na moto");
+        } else if (this.pessoa.idade < 2) {
+            System.out.println("Muito pequeno pra andar de moto");
+        } else if (this.pessoa.idade > 10) {
+            System.out.println("Muito grande pra andar de moto");
+        } else if (this.potencia == 1){
+            System.out.println(this.pessoa.nome + ": RUN....");
+        }else if  (this.potencia == 2){
+            System.out.println(this.pessoa.nome + ": RUN RUN.....");
+        }else if (this.potencia == 3){
+            System.out.println(this.pessoa.nome + ":RUN RUN RUN...");
+        }
+    }
+
+    public static void main(String[] args) {
+        Pessoa davi = new Pessoa("Davi", 10);
+        {
+            Motoca moto = new Motoca();
+            moto.embarcar(davi);
+            moto.dirigir();
+            moto.desembarcar();
+        }
+        Motoca moto2 = new Motoca();
+        moto2.embarcar(davi);
+        moto2.dirigir();
+        moto2.desembarcar();
+
+    }
+    /*
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Motoca motoca  = new Motoca();
@@ -80,10 +88,5 @@ public class Motoca {
 		}
 		scanner.close();
 	}
-*/
+     */
 }
-
-
-
-
-
