62ac5c37c167113328ac1c61839174599aeb904c
abnerhakinnen <abnerhakinnen@alu.ufc.br>
2020-03-11 19:04:19 -0300

    atividade02

--- ATV02/papelaria/src/papelaria/Lapiseira.java
+++ ATV02/papelaria/src/papelaria/Lapiseira.java
@@ -1,68 +1,62 @@
-
 package papelaria;
 
 public class Lapiseira {
-    public String marca;
-	public double ponta;
-	public Grafitte grafite;
-	
-	public Lapiseira(double ponta, String marca) {
-		super();
-		this.ponta = ponta;
-		this.marca = marca;
-	}
-	
-	public void escrever(int qtd) {
-		if(this.grafite.tamanhoGrafite <0) {
-			grafite = null;
-			System.out.println("fail: nao tem grafite");
-			return;
-		}
-		if(this.grafite.tamanhoGrafite > 0)
-			this.grafite.tamanhoGrafite -= qtd;
-		System.out.println("Escrevendo");
-	}
-	
-	public void colocarGrafite(Grafitte grafite) {
-		if(this.ponta == ponta) {
-			this.grafite = grafite;
-		}else {
-			System.out.println("fail: ponta incompativel");
-		}
-	}
-		
-	
-	public double getPonta() {
-		return ponta;
-	}
-	public void setPonta(double ponta) {
-		this.ponta = ponta;
-	}
-	public String getMarca() {
-		return marca;
-	}
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-	public int getTamanhoGrafite() {
-		return grafite.tamanhoGrafite;
-	}
-	public void setTamanhoGrafite(int tamanhoGrafite) {
-		this.grafite.tamanhoGrafite = tamanhoGrafite;
-	}
-	
-	@Override
-	public String toString() {
-		return "marca:" + this.marca + ", ponta:" + this.ponta + ", gtam:" + this.grafite.tamanhoGrafite;
-	}
-	
-	
-	public static void main(String[] args) {
-		Grafitte faber = new Grafitte (10, 0.7);
-		Lapiseira pentel = new Lapiseira(0.7, "Pentel");
-		pentel.escrever(4);
-		pentel.colocarGrafite(faber);
-		pentel.escrever(2);
-		System.out.println(pentel);
-	}
+
+    public double ponta;
+    public Grafitte grafite;
+
+    public Lapiseira(double ponta, String marca) {
+        super();
+        this.ponta = ponta;
+    }
+
+    public void escrever(int qtd) {
+        if (this.grafite.tamanhoGrafite < 0) {
+            grafite = null;
+            System.out.println("Coloque grafite");
+            return;
+        }
+        if (this.grafite.tamanhoGrafite > 0) {
+            this.grafite.tamanhoGrafite -= qtd;
+        }
+        System.out.println("Escrevendo");
+    }
+
+    public void colocarGrafite(Grafitte grafite) {
+        if (this.ponta == grafite.ponta) {
+            this.grafite = grafite;
+        } else {
+            System.out.println("Essa ponta não serve para essa grafite");
+        }
+    }
+
+    public double getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(double ponta) {
+        this.ponta = ponta;
+    }
+
+    public int getTamanhoGrafite() {
+        return grafite.tamanhoGrafite;
+    }
+
+    public void setTamanhoGrafite(int tamanhoGrafite) {
+        this.grafite.tamanhoGrafite = tamanhoGrafite;
+    }
+
+    @Override
+    public String toString() {
+        return "ponta:" + this.ponta + ", gtam:" + this.grafite.tamanhoGrafite;
+    }
+
+    public static void main(String[] args) {
+        Grafitte faber = new Grafitte(10, 0.7);
+        Lapiseira pentel = new Lapiseira(0.7, "Pentel");
+        pentel.escrever(4);
+        pentel.colocarGrafite(faber);
+        pentel.escrever(2);
+        System.out.println(pentel);
+    }
 }
